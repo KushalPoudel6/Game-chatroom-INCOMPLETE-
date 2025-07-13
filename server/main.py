81ba39b92d03017e6f386d6c9b2b6a612e2decd8
David <davembertels@gmail.com>
2025-07-13 15:16:11 -0400

    Moved server-side Pong code to pong.py.

--- server/main.py
+++ server/main.py
@@ -2,114 +2,63 @@ import asyncio
 from websockets.asyncio.server import broadcast, serve
 import json
 import secrets
+import pong
 
 HOST_INDEX = 0
 LOBBIES = {}
 
 class Player:
-    def __init__(self, socket, username):
-        self.socket = socket
+    def __init__(self, username):
         self.username = username
+        self.local_msg = None
 
 class Lobby:
     def __init__(self, code, host_socket, host_username):
         self.code = code
         self.connected = [host_socket]
-        self.players = [host_username]
+        self.players = [Player(host_username)]
 
-class Pong:
-    def reset(self, starter_index):
-        self.ball_x = 0.0
-        self.ball_y = 0.0
-        if starter_index == 0:
-            self.ball_vel_x = -1.0
-        else:
-            self.ball_vel_x = 1.0
-        self.ball_vel_y = 0.0
-        self.player_y = [0.0, 0.0]
-        self.player_vel_y = [0.0, 0.0]
+    # Returns player_index
+    def add_player(self, socket, username):
+        self.connected.append(socket)
+        self.players.append(Player(username))
+        return len(self.connected) - 1
 
-    def __init__(self):
-        self.reset(0)
+    def usernames(self):
+        return [x.username for x in self.players]
 
-class AABB:
-    def __init__(self, pos_x, pos_y, scale_x, scale_y):
-        self.x = pos_x - scale_x / 2
-        self.y = pos_y - scale_y / 2
-        self.width = scale_x
-        self.height = scale_y
-
-def overlap(aabb1, aabb2):
-    return aabb1.x < aabb2.x + aabb2.width and aabb1.x + aabb1.width > aabb2.x and \
-            aabb1.y < aabb2.y + aabb2.height and aabb1.y + aabb1.height > aabb2.y
-
-async def update(l):
-    delta_time = 1 / 64
+# Select either a local message or a websocket message
+async def select_message(l, player_index):
     while True:
-        l.game.ball_x += l.game.ball_vel_x * delta_time
-        l.game.ball_y += l.game.ball_vel_y * delta_time
-        l.game.player_y[0] += l.game.player_vel_y[0] * delta_time
-        l.game.player_y[1] += l.game.player_vel_y[1] * delta_time
-        ball_scale = 0.06
-        ball_top = l.game.ball_y + ball_scale / 2.0
-        ball_bottom = l.game.ball_y - ball_scale / 2.0
-        ball_right = l.game.ball_x + ball_scale / 2.0
-        ball_left = l.game.ball_x - ball_scale / 2.0
-        if ball_top >= 1.0 or ball_bottom <= -1.0:
-            l.game.ball_vel_y *= -1.0
+        if l.players[player_index].local_msg != None:
+            json_string = l.players[player_index].local_msg
+            # Consume message
+            l.players[player_index].local_msg = None
+            return json_string
+        try:
+            # 1 millisecond timeout
+            async with asyncio.timeout(0.001):
+                return await l.connected[player_index].recv()
+        except TimeoutError:
+            pass
 
-        influence = 0.5
-        paddle_x_scale = 0.04
-        paddle_y_scale = 0.6
-        left_paddle_aabb = AABB(-0.9, l.game.player_y[0], paddle_x_scale, paddle_y_scale)
-        ball_aabb = AABB(l.game.ball_x, l.game.ball_y, ball_scale, ball_scale)
-        if overlap(left_paddle_aabb, ball_aabb):
-            l.game.ball_vel_x *= -1.0
-            l.game.ball_vel_y += l.game.player_vel_y[0] * influence
-
-        right_paddle_aabb = AABB(0.9, l.game.player_y[1], paddle_x_scale, paddle_y_scale)
-        if overlap(right_paddle_aabb, ball_aabb):
-            l.game.ball_vel_x *= -1.0
-            l.game.ball_vel_y += l.game.player_vel_y[1] * influence
-
-        if ball_right >= 1.0:
-            print("player1 point")
-            # TODO
-            l.game.reset(1)
-        elif ball_left <= -1.0:
-            print("player2 point")
-            # TODO
-            l.game.reset(0)
-
-        msg = json.dumps({
-            "type":         "game_tick",
-            "player1_y":    l.game.player_y[0],
-            "player2_y":    l.game.player_y[1],
-            "ball_x":       l.game.ball_x,
-            "ball_y":       l.game.ball_y,
-        })
-        broadcast(l.connected, msg)
-        await asyncio.sleep(delta_time)
+def local_broadcast(l, msg):
+    for player in l.players:
+        player.local_msg = msg
 
 async def lobby(l, player_index):
-    async for json_string in l.connected[player_index]:
-        #print("Received message:")
-        #print(json_string)
+    while True:
+        json_string = await select_message(l, player_index)
         msg = json.loads(json_string)
         if msg["type"] == "chat":
             broadcast(l.connected, json_string)
         elif msg["type"] == "start_game" and player_index == HOST_INDEX:
-            l.game = Pong()
-            asyncio.ensure_future(update(l))
             broadcast(l.connected, json_string)
-        elif msg["type"] == "pong_move":
-            index = msg["player_index"]
-            if msg["action"] == "up":
-                l.game.player_vel_y[index] = 1.0
-            elif msg["action"] == "down":
-                l.game.player_vel_y[index] = -1.0
-            else:
-                l.game.player_vel_y[index] = 0
+            local_msg = json.dumps({ "type": "local_start_game" })
+            local_broadcast(l, local_msg)
+            pong.start(l)
+        elif msg["type"] == "local_start_game":
+            await pong.run(l, player_index)
 
 async def create(socket, username):
     code = secrets.token_urlsafe(12)
@@ -130,14 +79,12 @@ async def join(code, socket, username):
         "username": username,
     })
     broadcast(l.connected, msg)
-    player_index = len(l.connected)
-    l.connected.append(socket)
-    l.players.append(username)
+    player_index = l.add_player(socket, username)
     resp = json.dumps({
         "type":     "join_success",
         "code":     code,
         "username": username,
-        "players":  l.players,
+        "players":  l.usernames(),
     })
     await socket.send(resp)
     await lobby(l, player_index)
---
+++ server/pong.py
@@ -0,0 +1,95 @@
+import asyncio
+from websockets.asyncio.server import broadcast
+import json
+
+class Pong:
+    def reset(self, starter_index):
+        self.ball_x = 0.0
+        self.ball_y = 0.0
+        if starter_index == 0:
+            self.ball_vel_x = -1.0
+        else:
+            self.ball_vel_x = 1.0
+        self.ball_vel_y = 0.0
+        self.player_y = [0.0, 0.0]
+        self.player_vel_y = [0.0, 0.0]
+
+    def __init__(self):
+        self.reset(0)
+
+class AABB:
+    def __init__(self, pos_x, pos_y, scale_x, scale_y):
+        self.x = pos_x - scale_x / 2
+        self.y = pos_y - scale_y / 2
+        self.width = scale_x
+        self.height = scale_y
+
+def overlap(aabb1, aabb2):
+    return aabb1.x < aabb2.x + aabb2.width and aabb1.x + aabb1.width > aabb2.x and \
+            aabb1.y < aabb2.y + aabb2.height and aabb1.y + aabb1.height > aabb2.y
+
+# Only call this once, when lobby host starts the game
+def start(l):
+    l.game = Pong()
+    asyncio.ensure_future(update(l))
+
+async def run(l, player_index):
+    # NOTE: In the future we might want select_message instead
+    async for json_string in l.connected[player_index]:
+        msg = json.loads(json_string)
+        if msg["type"] == "pong_move":
+            index = msg["player_index"]
+            if msg["action"] == "up":
+                l.game.player_vel_y[index] = 1.0
+            elif msg["action"] == "down":
+                l.game.player_vel_y[index] = -1.0
+            else:
+                l.game.player_vel_y[index] = 0
+
+async def update(l):
+    delta_time = 1 / 64
+    while True:
+        l.game.ball_x += l.game.ball_vel_x * delta_time
+        l.game.ball_y += l.game.ball_vel_y * delta_time
+        l.game.player_y[0] += l.game.player_vel_y[0] * delta_time
+        l.game.player_y[1] += l.game.player_vel_y[1] * delta_time
+        ball_scale = 0.06
+        ball_top = l.game.ball_y + ball_scale / 2.0
+        ball_bottom = l.game.ball_y - ball_scale / 2.0
+        ball_right = l.game.ball_x + ball_scale / 2.0
+        ball_left = l.game.ball_x - ball_scale / 2.0
+        if ball_top >= 1.0 or ball_bottom <= -1.0:
+            l.game.ball_vel_y *= -1.0
+
+        influence = 0.5
+        paddle_x_scale = 0.04
+        paddle_y_scale = 0.6
+        left_paddle_aabb = AABB(-0.9, l.game.player_y[0], paddle_x_scale, paddle_y_scale)
+        ball_aabb = AABB(l.game.ball_x, l.game.ball_y, ball_scale, ball_scale)
+        if overlap(left_paddle_aabb, ball_aabb):
+            l.game.ball_vel_x *= -1.0
+            l.game.ball_vel_y += l.game.player_vel_y[0] * influence
+
+        right_paddle_aabb = AABB(0.9, l.game.player_y[1], paddle_x_scale, paddle_y_scale)
+        if overlap(right_paddle_aabb, ball_aabb):
+            l.game.ball_vel_x *= -1.0
+            l.game.ball_vel_y += l.game.player_vel_y[1] * influence
+
+        if ball_right >= 1.0:
+            print("player1 point")
+            # TODO
+            l.game.reset(1)
+        elif ball_left <= -1.0:
+            print("player2 point")
+            # TODO
+            l.game.reset(0)
+
+        msg = json.dumps({
+            "type":         "game_tick",
+            "player1_y":    l.game.player_y[0],
+            "player2_y":    l.game.player_y[1],
+            "ball_x":       l.game.ball_x,
+            "ball_y":       l.game.ball_y,
+        })
+        broadcast(l.connected, msg)
+        await asyncio.sleep(delta_time)
